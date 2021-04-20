db0832c74a5491b027388af7c7e5434426dc7b83
Darshan Patel <lazy14k@gmail.com>
2021-04-20 23:59:06 +0530

    Made downloder user friendly

--- bin/downloader.js
+++ bin/downloader.js
@@ -3,42 +3,40 @@
 const request = require('request-promise-native')
 const _ = require('lodash')
 const fs = require('fs')
+const readline = require('readline')
 const cliProgress = require('cli-progress')
-const { argv } = require('yargs/yargs')(process.argv.slice(2))
-    .usage('Usage: $0 -c [num] -u [url]')
-    .demandOption(['c', 'u'])
-    .option('c', {
-        describe: 'Parallel connection',
-        type: 'number',
-        default: 8,
-    })
-    .option('u', {
-        describe: 'URL to fetch file uri',
-        type: 'url',
-        required: true,
-    })
 
-const concurrency = argv.c
+const concurrency = parseInt(process.env.C, 10) || 8
 
 const getProgressBar = fileName => new cliProgress.SingleBar({
     format: `Downloading ${fileName} | {bar} {percentage}% || {value}/{total} Parts || ETA: {eta_formatted} / {duration_formatted} `,
 }, cliProgress.Presets.shades_classic)
 
-const parseURI = async () => {
-    const uri = await request.get(argv.u)
+const parseURI = async (url) => {
+    const uri = await request.get(url)
 
     return JSON.parse(Buffer.from(uri, 'base64')
         .toString())
 }
 
 const downloader = async () => {
-    let data
-    try {
-        data = await parseURI()
-    } catch (err) {
-        console.log('=== could not parse uri!')
-        process.exit(1)
-    }
+    const data = await new Promise((resolve) => {
+        const i = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        })
+        i.question('> Enter file uri : ', async (url) => {
+            try {
+                const parsedData = await parseURI(url)
+                i.close()
+                process.stdin.destroy()
+                resolve(parsedData)
+            } catch (err) {
+                console.log('=== could not parse uri')
+                process.exit(1)
+            }
+        })
+    })
     const { fileName, files } = data
     const downloadFilePath = `${__dirname}/${fileName}`
     const stream = fs.createWriteStream(downloadFilePath)
