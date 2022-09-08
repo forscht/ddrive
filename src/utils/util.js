b7573e3a17521308d538d10f22e1fe79071b4ca7
BrainBursty <112560814+BrainBursty@users.noreply.github.com>
2022-09-08 11:35:30 +0200

    chore!: remove duplicate util function

--- src/utils/util.js
+++ src/utils/util.js
@@ -93,35 +93,6 @@ class Util {
     }
 
     /**
-     * Convert bytes to human readable format
-     * @param bytes
-     * @param si
-     * @param dp
-     * @return {string}
-     */
-    static humanFileSize(bytes, si = false, dp = 1) {
-        const thresh = si ? 1000 : 1024
-
-        if (Math.abs(bytes) < thresh) {
-            return `${bytes} B`
-        }
-
-        const units = si
-            ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-            : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
-        let u = -1
-        const r = 10 ** dp
-
-        do {
-            // eslint-disable-next-line no-param-reassign
-            bytes /= thresh
-            u += 1
-        } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1)
-
-        return `${bytes.toFixed(dp)} ${units[u]}`
-    }
-
-    /**
      * Sort array of object by key
      * @param {Object[]} array
      * @param {String} key
