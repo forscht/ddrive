fbfd80de91af1e563e7663e062ff933a362c15fb
Darshan Hihoriya <iroguesniper@gmail.com>
2023-01-08 09:23:34 +0530

    feat: fix bug

--- src/DFs/lib/AsyncStreamProcessor.js
+++ src/DFs/lib/AsyncStreamProcessor.js
@@ -1,26 +1,47 @@
 const { Transform } = require('stream')
 
+function cbNoop(cb) {
+    cb()
+}
+
 class AsyncStreamProcessor extends Transform {
-    constructor(chunkProcessor, maxConcurrency) {
+    constructor(chunkProcessor, maxConcurrency = 1) {
         super()
         this.chunkProcessor = chunkProcessor
-        this.maxConcurrency = maxConcurrency || 3
-        this.currConcurrency = 1
+        this.maxConcurrency = maxConcurrency
         this.chunkCount = 0
+
+        this.lastCallback = undefined
+        this.pendingFinish = undefined
+        this.concurrent = 0
+        this._final = this.callOnFinish(cbNoop)
+    }
+
+    callOnFinish(original) {
+        return function cbHell(callback) {
+            if (this.concurrent === 0) original.call(this, callback)
+            else this.pendingFinish = original.bind(this, callback)
+        }
     }
 
     _transform(chunk, encoding, callback) {
-        let callBackCalled = false
-        if (this.currConcurrency < this.maxConcurrency) {
+        this.concurrent += 1
+        if (this.concurrent < this.maxConcurrency) {
             callback(null)
-            this.currConcurrency += 1
-            callBackCalled = true
-        }
+        } else this.lastCallback = callback
         this.chunkProcessor(chunk, this.chunkCount)
             .then(() => {
-                if (!callBackCalled) callback(null)
+                this.concurrent -= 1
+                if (this.lastCallback) {
+                    this.lastCallback()
+                    this.lastCallback = null
+                }
+                if (this.concurrent === 0 && this.pendingFinish) {
+                    this.pendingFinish()
+                    this.pendingFinish = null
+                }
             })
-            .catch((err) => callback(err))
+            .catch((err) => this.emit('error', err))
         this.chunkCount += 1
     }
 }
